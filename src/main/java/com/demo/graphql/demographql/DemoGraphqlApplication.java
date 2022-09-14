6e1227974123384c09465a927d711cdd6b0f0b61
yurid <yurid@playtika.com>
2022-09-14 13:53:26 +0300

    annotation based approach

--- src/main/java/com/demo/graphql/demographql/DemoGraphqlApplication.java
+++ src/main/java/com/demo/graphql/demographql/DemoGraphqlApplication.java
@@ -2,11 +2,55 @@ package com.demo.graphql.demographql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+import java.util.Objects;
 
 @SpringBootApplication
 public class DemoGraphqlApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoGraphqlApplication.class, args);
+    }
+
+    @Controller
+    class GreetingsController {
+        private final List<Customer> CUSTOMERS = List.of(new Customer(1, "Serega"), new Customer(2, "Ivan"), new Customer(3, "Yura"));
+
+        @QueryMapping
+        String helloWithName(@Argument String name) {
+            return "Hello, " + name + "!";
+        }
+
+
+        @QueryMapping
+            //@SchemaMapping(typeName = "Query", field = "hello")
+        String hello() {
+            return "Hello, world!";
+        }
+
+        @QueryMapping
+        Customer customerById(@Argument Integer id) {
+            return CUSTOMERS.stream().filter(customer -> Objects.equals(customer.id, id)).findFirst().orElseThrow();
+        }
+
+        @QueryMapping
+        //or Flux<Customer>
+        List<Customer> customers() {
+            return CUSTOMERS;
+        }
+    }
+
+    public static class Customer {
+        Integer id;
+        String name;
+
+        public Customer(Integer id, String name) {
+            this.id = id;
+            this.name = name;
+        }
     }
 }
